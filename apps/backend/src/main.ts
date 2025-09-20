f7934740d5b1a6e249f5b5a14ec95d5f91ea947e
malikakika <malikachoubri@gmail.com>
2025-09-20 19:48:57 +0200

    update docker yml

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -4,7 +4,13 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: ['http://localhost:4200'], credentials: true });
+app.enableCors({ 
+  origin: [
+    'http://localhost:4200',
+    'https://fileor.roadshine.ma'
+  ],
+  credentials: true 
+});
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   await app.listen(process.env.PORT ? +process.env.PORT : 3333);
